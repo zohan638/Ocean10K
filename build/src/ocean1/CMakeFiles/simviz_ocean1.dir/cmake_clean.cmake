c86dffd46941636b093819a247496a2de33c5826
Tasha Kim <tashakim@google.com>
2024-05-23 18:21:09 -0700

    Set up ocean one controller with urdf and source directory

---
+++ build/src/ocean1/CMakeFiles/simviz_ocean1.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/tashakim/CS225A-Project/bin/ocean1_example/simviz_ocean1"
+  "/Users/tashakim/CS225A-Project/bin/ocean1_example/simviz_ocean1.pdb"
+  "CMakeFiles/simviz_ocean1.dir/simviz.cpp.o"
+  "CMakeFiles/simviz_ocean1.dir/simviz.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/simviz_ocean1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/ocean1/CMakeFiles/simviz_ocean1.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for simviz_ocean1.
+# This may be replaced when dependencies are built.
