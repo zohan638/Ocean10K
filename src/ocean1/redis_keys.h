6e5aacc79bbb093e807a78136417fc8704aa8cf3
Tasha Kim <tashakim@google.com>
2024-06-01 02:13:41 -0700

    Add left and right simulated forces as redis keys

--- src/ocean1/redis_keys.h
+++ src/ocean1/redis_keys.h
@@ -3,4 +3,5 @@ const std::string JOINT_ANGLES_KEY = "sai2::sim::ocean1::sensors::q";
 const std::string JOINT_VELOCITIES_KEY = "sai2::sim::ocean1::sensors::dq";
 const std::string JOINT_TORQUES_COMMANDED_KEY = "sai2::sim::ocean1::actuators::fgc";
 const std::string CONTROLLER_RUNNING_KEY = "sai2::sim::ocean1::controller";
-
+const std::string SIMULATED_COMMANDED_FORCE_KEY_SUFFIX_LEFT = "sai2::sim::ocean1::simlated_forces_left";
+const std::string SIMULATED_COMMANDED_FORCE_KEY_SUFFIX_RIGHT = "sai2::sim::ocean1::simlated_forces_right";
